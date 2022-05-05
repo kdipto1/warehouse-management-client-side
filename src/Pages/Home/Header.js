caf4e1f692593c089514d9215763d2782cf5dd61
kdipto1 <kdipto@outlook.com>
2022-05-05 21:55:26 +0600

    website tittle changes

--- src/Pages/Home/Header.js
+++ src/Pages/Home/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand as={Link} to="/">
-            React-Bootstrap
+            CarHouse
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
