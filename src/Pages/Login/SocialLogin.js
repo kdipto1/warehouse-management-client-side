98032a6e324c13c36c9605484459acbc0f68daae
kdipto1 <kdipto@outlook.com>
2022-05-08 22:08:11 +0600

    server address changed to heroku

--- src/Pages/Login/SocialLogin.js
+++ src/Pages/Login/SocialLogin.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-import Skeleton from 'react-loading-skeleton';
 import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import auth from "../../firebase.init";
 import "./SocialLogin.css";
 import { ImGoogle3 } from "react-icons/im";
+import Loading from '../Utility/Loading';
 
 const SocialLogin = () => {
   const navigate = useNavigate();
@@ -17,7 +17,7 @@ const SocialLogin = () => {
       navigate(from, { replace: true });
     }
     if (loading) {
-      <Skeleton count={6} />
+      <Loading/>
       return;
     }
     if (error) {
