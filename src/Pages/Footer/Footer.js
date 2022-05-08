ae706490d70162b6a20f7081ba196a4ee170607b
kdipto1 <kdipto@outlook.com>
2022-05-08 19:56:53 +0600

    fotter section added

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
   return (
-    <div>
-      <h2 className="text-center">Footer</h2>
+    <div className="text-center bg-black text-white mt-3 py-2">
+      <h6>Dipto &copy; {year}</h6>
+      <h2>
+        <FaFacebook /> <FaInstagram /> <FaTwitter />
+      </h2>
     </div>
   );
 };
